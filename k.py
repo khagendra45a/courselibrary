3300f91370b056765d0b7147d9def7f9f6afadff
khagendra45a <k97164577@gmail.com>
2024-06-01 10:44:31 +0545

    Add files via upload

---
+++ k.py
@@ -0,0 +1,6 @@
+
+a=6#string 
+print(a+a)
+
+# here both are string in python so its work in string format ,we indicate as in a apstrope brackest 
+#in python the variable decleration is not bbe possibele when th epath is nit be declered by the formet in the form is nt ne the definrf by the form is not be the fomr is not 
